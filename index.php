c2e14f676e39c468373fd491ccea3ba3197ebcdc
Matthew Sanders <mms578@g.harvard.edu>
2016-09-20 14:07:11 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Matthew Sanders - Project Two</title>
+	<meta charset='utf-8'>
+
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+<h1>Placeholder?</h1>
+
+</body>
+</html>
